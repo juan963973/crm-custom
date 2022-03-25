045940ff96fd8be43c3652a3ac83eb2f5503b4d8
Jonattan Miranda <jonattan.miranda@bancontinental.com.py>
2022-03-25 11:40:11 -0300

    Actualizacion semanal de tarea.

--- src/components/_common/date-field.tsx
+++ src/components/_common/date-field.tsx
@@ -3,7 +3,29 @@ import DateRangePicker from 'rsuite/DateRangePicker';
 import "rsuite/dist/rsuite.min.css";
 
 const Datefield: any = (param:any) => {
-  return <DateRangePicker onChange={param.onChange} id={param.keyFilter} /> /*<FormControl type="date" className="mt-1" size="sm" onChange={param.onChange} id={param.keyFilter} />*/;
+  const handleChange = (e: any) =>{
+    if(Object.keys(e).length === 0) return;
+    const result = {
+      target :   {
+        id: param.keyFilter, 
+        value: {
+        from: dateFormat(e[0]),
+        to: dateFormat(e[1])
+      }}
+    }
+    console.log(dateFormat(new Date))
+    param.onChange(result)
+  }
+
+  const dateFormat = (date:any)=>{
+    var day = `0${date.getDate()}`.slice(-2); //("0"+date.getDate()).slice(-2);
+    var month = `0${date.getMonth() + 1}`.slice(-2);
+    var year = date.getFullYear();
+    
+    return `${year}-${month}-${day}`
+  }
+
+  return <DateRangePicker onChange={handleChange}  /> /*<FormControl type="date" className="mt-1" size="sm" onChange={param.onChange} id={param.keyFilter} />*/;
 };
 
 export default Datefield;
