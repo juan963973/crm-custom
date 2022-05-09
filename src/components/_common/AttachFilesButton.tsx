43ee16fe88ab49c10a43fe1de2592b631478e2e5
Cristhian <cristhian.chavez@bancontinental.com.py>
2022-05-09 10:09:12 -0400

    .

--- src/components/_common/AttachFilesButton.tsx
+++ src/components/_common/AttachFilesButton.tsx
@@ -1,34 +1,48 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 import AttachFilesLoad from "./AttachFilesLoad";
+import Child from './prueba'
 
-export default function AttachFilesButton(){
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+export default function AttachFilesButton() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    return (
-        <>
-          <Button variant="primary" className="btn btn-primary btn-sm" onClick={handleShow}>
+  const childRef = useRef();
+
+  const submitButton = () => {
+    childRef.current.getAlert()
+    handleClose()
+  }
+  
+
+  return (
+    <>
+      <Button variant="primary" className="btn btn-primary btn-sm" onClick={handleShow}>
+        Adjuntar
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Adjuntar archivo</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <AttachFilesLoad />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancelar
+          </Button>
+          <Button variant="primary" onClick={submitButton}>
+          <Child ref={childRef} />
             Adjuntar
           </Button>
-    
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Adjuntar archivo</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <AttachFilesLoad show={show}/>
-              </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Cancelar
-              </Button>
-              <Button variant="primary" onClick={handleClose}>
-                Adjuntar
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </>
-      );
-    }
+        </Modal.Footer>
+        {/* <div>
+          <Child ref={childRef} />
+          <button onClick={() => childRef.current.getAlert()}>Click</button>
+        </div> */}
+      </Modal>
+    </>
+  );
+}
