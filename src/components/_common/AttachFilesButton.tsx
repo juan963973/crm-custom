2de0b1ff7308a2e8a98e6cb00690270dcb3eb867
Cristhian <cristhian.chavez@bancontinental.com.py>
2022-05-10 14:12:37 -0400

    AttachFiles component improvements

--- src/components/_common/AttachFilesButton.tsx
+++ src/components/_common/AttachFilesButton.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import React, { useCallback, useRef, useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+import { Button, Col, Modal, Row } from "react-bootstrap";
 import { useDropzone } from 'react-dropzone';
-import importFile from "services/attachmentFilesService";
+import importFile from "services/attachService";
 
 export default function AttachFilesButton() {
   const [show, setShow] = useState(false);
@@ -19,59 +19,66 @@ export default function AttachFilesButton() {
       importFile(id, file)
       console.log('formData', formData)
     }
-
-    // axios({
-    //   method: 'post',
-      
-    //   data: formData,
-
-    //   })
-      
-    alert('siuuu')
   }
 
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({    
+    maxFiles:2,
+    accept: {
+      'image/png': ['.png'],
+      'image/png': ['.jpg'],
+      'text/html': ['.html', '.htm'],
+    }
+  });
 
   // console.log(props.id)
   const files = acceptedFiles.map(file => (
-    <li key={file.path}>
-      {file.path}
+    <Row key={file.path}>
+      <Col sm={1}><img src="/foldericon.png" width="20" height="20"  style={{marginRight: '10x'}}/></Col>
+      <Col>{file.path}
       - {file.size}
-      bytes
-    </li>
+      bytes</Col>
+    </Row>
   ));
 
   const submitButton = () => {
     uploadFiles()
-
     handleClose()
   }
-
 
   return (
     <>
       <Button variant="primary" className="btn btn-primary btn-sm" onClick={handleShow}>
         Adjuntar
       </Button>
-
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Adjuntar archivo</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <section className="container">
-            <div {...getRootProps({ className: 'dropzone' })}>
+            <div {...getRootProps({ className: 'dropzone' })}
+            style={{
+              color: 'gray',
+              marginBottom: 10,
+              marginTop: 10,
+              borderColor: 'rgb(237, 240, 244)',
+              borderStyle: 'dashed',
+              borderWidth: '2px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              alignSelf: 'ceneter',
+              height: '80px'
+            }}
+            >
               <input {...getInputProps()} />
-              <p>Drag 'n' drop some files here, or click to select files</p>
+              <p>Haga clic aquí o suelte los archivos para adjuntarlos.</p>
             </div>
             {files.length > 0 && <React.Fragment>
               <div>
-                <h4>Files</h4>
                 <ul>{files}</ul>
               </div>
-              {/* <button>Submit</button> */}
             </React.Fragment>}
-
           </section>
         </Modal.Body>
         <Modal.Footer>
@@ -79,14 +86,9 @@ export default function AttachFilesButton() {
             Cancelar
           </Button>
           <Button variant="primary" onClick={submitButton}>
-            {/* <Child ref={childRef} /> */}
             Adjuntar
           </Button>
         </Modal.Footer>
-        {/* <div>
-          <Child ref={childRef} />
-          <button onClick={() => childRef.current.getAlert()}>Click</button>
-        </div> */}
       </Modal>
     </>
   );
--- src/services/attachService.ts
+++ src/services/attachService.ts
@@ -1,10 +1,5 @@
 import axios from "axios"
 
-// export default async function importFile(id) {
-//     const res = await axios.post(`https://localhost:5001/v1/api/Attachment?module=Case&moduleId=${id}`
-//     alert('hello')
-// }
-
 const importFile= async (id: any, file: any) => {
     const formData = new FormData();
     formData.append("file", file);
@@ -13,10 +8,9 @@ const importFile= async (id: any, file: any) => {
         moduleId: id,
         file: formData
     }
-    console.log("mmm",attachmentModel)
     try {
         const res = await axios.post(`https://localhost:5001/v1/api/Attachment?module=Case&moduleId=${id}`, formData);
-        alert('success')
+        console.log('success attach file')
     } catch (ex) {
         console.log(ex);
     }
