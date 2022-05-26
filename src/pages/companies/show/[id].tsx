6df2550d3aa528b706b2e85b079dc6eaca99a34b
CRS-CLTPY <crsth.1990@fpuna.edu.py>
2022-05-26 14:33:35 -0400

    styling into header of company details page

--- src/pages/companies/show/[id].tsx
+++ src/pages/companies/show/[id].tsx
@@ -1,6 +1,6 @@
 import {
     Button, Col, Container, Form, Row, Card, InputGroup, FormControl,
-    ButtonGroup, ToggleButton, Nav, Tabs, Tab
+    ButtonGroup, ToggleButton, Nav, Tabs, Tab, DropdownButton, Dropdown
 } from "react-bootstrap";
 import { useRouter } from 'next/router'
 import { CaseDetailModel } from ".../../models/Case";
@@ -26,7 +26,7 @@ export default function Show() {
     return (
         <>
 
-            <Container className="shadow-sm p-3 mb-3 bg-white rounded mt-2">
+            {/* <Container className="shadow-sm p-3 mb-3 bg-white rounded mt-2">
                 <Row style={{marginTop: '50px'}}>
                     <Col sm={1} style={{width: '30px'}} >
                         <h4>
@@ -49,6 +49,35 @@ export default function Show() {
                         <Button variant="secondary">...</Button>{' '}
                         «
                         »
+                    </Col>
+                </Row>
+            </Container> */}
+
+            <Container className="shadow-sm p-3 mb-3 bg-white rounded mt-2">
+            <Row style={{ marginTop: '50px', display: 'flex', alignItems: 'start', height: '30px' }}>
+                    <Col sm={1} style={{ width: '50px'}}>
+                        <img src="/backIcon.png" alt="Atrás" height="20" onClick={() => { window.history.back() }} />
+                    </Col>
+                    
+                    <Col  sm={1} style={{ width: '70px'}}>
+                        <img src="/compayIcon.png" style={{ height: '40px' }} />
+                    </Col>
+                    <Col sm={8} style={{ marginRight: 'auto'}}>
+
+                            <h4>Cabecera</h4>
+
+                    </Col>
+                    <Col sm={2} style={{ marginLeft: 'auto', display: 'flex', justifyContent: 'flex-end' }} align='end'>
+                        <Row align='end' style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                            <Col > <Button variant="secondary" style={{background: '#FFF', color: 'black' }}>Edit</Button>{' '}</Col>
+                            <Col>
+                                <DropdownButton variant="secondary"align="end" id="dropdown-basic-button" title="..." style={{background: '#FFF', color: 'black' }}>
+                                    <Dropdown.Item href="#/action-1">Clonar</Dropdown.Item>
+                                    <Dropdown.Item>Eliminar</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                                </DropdownButton>{" "}
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </Container>
@@ -74,7 +103,7 @@ export default function Show() {
 
                                 <Col sm={4} style={{ width: 'auto' }}>
 
-                                    <Button variant="primary">Resumen</Button>
+                                    <Button variant="primary">Visión general</Button>
                                     {/* 
                                     <Tabs defaultActiveKey="overview" id="uncontrolled-tab-example" className="flex-row" variant="pills">
                                         <Tab eventKey="overview" title="Overview">
