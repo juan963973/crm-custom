4db82f59d1487d6e66e532b54873a880d74a0990
Juan Andres Alfonso Gimenez <juan.alfonso@bancontinental.com.py>
2022-06-03 16:23:10 -0400

    ultimos cambios

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import type { ReactElement } from 'react';
+import type { ReactElement } from 'react'; 
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
