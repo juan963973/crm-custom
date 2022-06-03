a7920786fa4fce84cb20cf3a8d7e4ab7856e6f6e
cristhian-chavez <crsth.1990@gmail.com>
2022-06-03 16:46:51 -0400

    Integration ContactDetail's endpoint

--- src/components/_common/overview3.tsx
+++ src/components/_common/overview3.tsx
@@ -12,6 +12,35 @@ export default function Overview3({ dataIdCase, dataCompany, data }: any) {
                 <Row style={{ width: '99%' }}>
                     <Card body>
                         <Row className='mt-200' style={{ marginBottom: 20, marginTop: 20 }}>
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Imagen del contacto</Col>
+                                <Col align="start">
+                                    * imagen *
+                                </Col>
+                            </Row>
+
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Tipo de persona</Col>
+                                <Col align="start">
+                                     {data?.clientType ? data.clientType : ' - '}
+                                </Col>
+                            </Row>
+
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Título</Col>
+                                <Col align="start">
+                                     {data?.wwww ? data.wwww : ' arreglar '}
+                                </Col>
+                            </Row>
+                        </Row>
+                    </Card>
+                </Row>
+            </Row>
+
+            <Row style={{ alignItems: 'center', justifyContent: 'center', marginBottom: 15, marginTop: 15 }}>
+                <Row style={{ width: '99%' }}>
+                    <Card body>
+                        <Row className='mt-200' style={{ marginBottom: 20, marginTop: 20 }}>
                             <Row>
                                 <Col> <h6>DATOS BÁSICOS</h6> </Col>
                             </Row>
@@ -146,30 +175,237 @@ export default function Overview3({ dataIdCase, dataCompany, data }: any) {
                             </Row>
 
                         </Row>
+
+                        <Row className='mt-200' style={{ marginBottom: 20, marginTop: 20 }}>
+                            <Row>
+                                <Col> <h6>DIRECCIÓN DEL CONTACTO</h6> </Col>
+                            </Row>
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Dirección</Col>
+                                <Col align="start">
+                                    {data?.address ? data.address : ' - '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}>Nro. domicilio</Col>
+                                <Col sm={4} align="start">
+                                    {data?.homeNumber ? data.homeNumber : ' - '}
+                                </Col>
+                            </Row>
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Referencia</Col>
+                                <Col align="start">
+                                    {data?.reference ? data.reference : ' - '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}>Departamento</Col>
+                                <Col sm={4} align="start">
+                                    {data?.departmentName ? data.departmentName : ' - '}
+                                </Col>
+                            </Row>
+
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Ciudad</Col>
+                                <Col align="start">
+                                    {data?.cityName ? data.cityName : ' - '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}>Barrio</Col>
+                                <Col sm={4} align="start">
+                                    {data?.neighborhood ? data.neighborhood : ' - '}
+                                </Col>
+                            </Row>
+
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>¿Enviar correspondencia a esta dirección?</Col>
+                                <Col align="start">
+                                    {data?.noHay ? data.noHay : ' - '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}></Col>
+                                <Col sm={4} align="start"></Col>
+                            </Row>
+
+                        </Row>
+
+                        <Row className='mt-200' style={{ marginBottom: 20, marginTop: 20 }}>
+                            <Row>
+                                <Col> <h6>DIRECCIÓN PARA CORRESPONDENCIA</h6> </Col>
+                            </Row>
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Domicilio para correspondencia</Col>
+                                <Col align="start">
+                                    {data?.mailingStreet ? data.mailingStreet : ' - '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}>País para correspondencia</Col>
+                                <Col sm={4} align="start">
+                                    {data?.mailingCountry ? data.mailingCountry : ' - '}
+                                </Col>
+                            </Row>
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Estado para correspondencia</Col>
+                                <Col align="start">
+                                    {data?.mailingState ? data.mailingState : ' - '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}>Ciudad para correspondencia</Col>
+                                <Col sm={4} align="start">
+                                    {data?.mailingCity ? data.mailingCity : ' - '}
+                                </Col>
+                            </Row>
+                        </Row>
+
+                        <Row className='mt-200' style={{ marginBottom: 20, marginTop: 20 }}>
+                            <Row>
+                                <Col> <h6>DATOS LABORALES</h6> </Col>
+                            </Row>
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Lugar de trabajo</Col>
+                                <Col align="start">
+                                    {data?.workPlace ? data.workPlace : ' - '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}>Salario actual</Col>
+                                <Col sm={4} align="start">
+                                    {data?.currentSalary ? data.currentSalary : ' - '}
+                                </Col>
+                            </Row>
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Teléfono laboral</Col>
+                                <Col align="start">
+                                    {data?.workPhone ? data.workPhone : ' - '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}></Col>
+                                <Col sm={4} align="start"></Col>
+                            </Row>
+                        </Row>
+
+                        <Row className='mt-200' style={{ marginBottom: 20, marginTop: 20 }}>
+                            <Row>
+                                <Col> <h6>DATOS EMPRESA</h6> </Col>
+                            </Row>
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Nombre de empresa</Col>
+                                <Col align="start">
+                                    {data?.mmmmmm ? data.mmmmmm : ' Arreglar '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}>Empresas lookup</Col>
+                                <Col sm={4} align="start">
+                                    {data?.mmmmmm ? data.mmmmmm : ' Arreglar '}
+                                </Col>
+                            </Row>
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Actividad económica</Col>
+                                <Col align="start">
+                                    {data?.mmmmmm ? data.mmmmmm : ' Arreglar '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}>Descripción</Col>
+                                <Col sm={4} align="start">{data?.mmmmmm ? data.mmmmmm : ' Arreglar '}</Col>
+                            </Row>
+                        </Row>
+
+                        <Row className='mt-200' style={{ marginBottom: 20, marginTop: 20 }}>
+                            <Row>
+                                <Col> <h6>DESCRIPCIÓN DEL CONTACTO</h6> </Col>
+                            </Row>
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Descripción</Col>
+                                <Col align="start">
+                                    {data?.mmmmmm ? data.mmmmmm : ' Arreglar '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}></Col>
+                                <Col sm={4} align="start"></Col>
+                            </Row>
+                        </Row>
+
+                        <Row className='mt-200' style={{ marginBottom: 20, marginTop: 20 }}>
+                            <Row>
+                                <Col> <h6>ATENCIÓN</h6> </Col>
+                            </Row>
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Banca</Col>
+                                <Col align="start">
+                                    {data?.mmmmmm ? data.mmmmmm : ' Arreglar '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}>Sucursal BC</Col>
+                                <Col sm={4} align="start">
+                                    {data?.mmmmmm ? data.mmmmmm : ' Arreglar '}
+                                </Col>
+                            </Row>
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Gerente</Col>
+                                <Col align="start">
+                                    {data?.mmmmmm ? data.mmmmmm : ' Arreglar '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}>Team Leader</Col>
+                                <Col sm={4} align="start">{data?.mmmmmm ? data.mmmmmm : ' Arreglar '}</Col>
+                            </Row>
+
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Supervisor</Col>
+                                <Col align="start">
+                                    {data?.mmmmmm ? data.mmmmmm : ' Arreglar '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}>Oficial</Col>
+                                <Col sm={4} align="start">{data?.mmmmmm ? data.mmmmmm : ' Arreglar '}</Col>
+                            </Row>
+                        </Row>
+
+                        <Row className='mt-200' style={{ marginBottom: 20, marginTop: 20 }}>
+                            <Row>
+                                <Col> <h6>DATOS REGISTRO</h6> </Col>
+                            </Row>
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Creado por</Col>
+                                <Col align="start">
+                                    {data?.mmmmmm ? data.mmmmmm : ' Arreglar '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}>Propietario del contacto</Col>
+                                <Col sm={4} align="start"></Col>
+                            </Row>
+
+                            <Row align="end" style={{ marginBottom: '5px' }}>
+                                <Col style={{ color: 'gray' }}>Modificado por</Col>
+                                <Col align="start">
+                                    {data?.mmmmmm ? data.mmmmmm : ' Arreglar '}
+                                </Col>
+
+                                <Col style={{ color: 'gray' }}></Col>
+                                <Col sm={4} align="start"></Col>
+                            </Row>
+                        </Row>
                     </Card>
                 </Row>
             </Row>
 
             <Row style={{ alignItems: 'center', justifyContent: 'center', marginBottom: 15, marginTop: 15 }}>
-                {/*<Row style={{ width: '99%' }}>*/}
-                {/*    <Card body>*/}
-                {/*        <Row className='mt-200'>*/}
-                {/*            <Col align="start">*/}
-                {/*                <h6 id="cases">*/}
-                {/*                    Casos*/}
-                {/*                </h6>*/}
-                {/*            </Col>*/}
-                {/*            <Col align="end">*/}
-                {/*                /!* <NewButtonCase dataIdCase={dataIdCase} /> *!/*/}
-                {/*            </Col>*/}
-                {/*        </Row>*/}
-                {/*        <Row style={{*/}
-                {/*            justifyContent: 'between',*/}
-                {/*            alignItems: 'center'*/}
-                {/*        }}>*/}
-                {/*        </Row>*/}
-                {/*    </Card>*/}
-                {/*</Row>*/}
+                <Row style={{ width: '99%' }}>
+                    <Card body>
+                        <Row className='mt-200'>
+                            <Col align="start">
+                                <h6 id="cases">
+                                    Casos
+                                </h6>
+                            </Col>
+                            <Col align="end">
+                                 <NewButtonCase dataIdCase={dataIdCase} />
+                            </Col>
+                        </Row>
+                        <Row style={{
+                            justifyContent: 'between',
+                            alignItems: 'center'
+                        }}>
+                        </Row>
+                    </Card>
+                </Row>
             </Row>
         </>
     )
