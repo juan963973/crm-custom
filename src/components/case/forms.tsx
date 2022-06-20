3972beb34f149aa78c9594fd80c4741fe0cde29b
Juan Alfonso <juan.alfonso@clt.com.py>
2022-06-20 16:19:53 -0400

    habilitar cliente, cuando se selecciona empresas

--- src/components/case/forms.tsx
+++ src/components/case/forms.tsx
@@ -57,7 +57,7 @@ const Forms = ({
                 /> */}
               <CustomAsyncPaginate
                 searchEndpoint="contacts"
-                disabled={caseData?.companyId !== null}
+                // disabled={caseData?.companyId !== null}
                 keyFilter={"contactId"}
                 onChange={handleChange}
                 defaultValue={ {value: caseData.contactId, label: reference.fullName}}
@@ -81,7 +81,7 @@ const Forms = ({
               
               <CustomAsyncPaginate
                 searchEndpoint="companies"
-                disabled={caseData?.contactId !== null}
+                disabled={caseData?.contactId !== null && caseData?.companyId === null}
                 keyFilter={"companyId"}
                 onChange={handleChange}
                 defaultValue={{value: caseData.companyId, label: reference.name}}
